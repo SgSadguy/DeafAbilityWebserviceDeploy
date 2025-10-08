fa2e274c0989d766929b388f542fb4c8320b07fb
SgSadguy <tusspukk481@gmail.com>
2025-10-08 07:45:21 +0700

    Fix Setting

--- deafability/deafability/settings.py
+++ deafability/deafability/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-2%!pa-(#&-c%-q=p^f&v6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'deafabilitywebservicedeploy.onrender.com',
+]
 
 # Application definition
 
